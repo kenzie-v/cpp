88a682fada156b894a670b4e9f6b923792df3b04
kenzie-v <bluedreamblueshift@gmail.com>
2026-08-16 20:58:51 -0700

    Types

---
+++ OTHER/Types/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main() {
+    std::cout<<"int: "<<sizeof(int)<<std::endl;
+    std::cout<<"float: "<<sizeof(float)<<std::endl;
+    std::cout<<"double: "<<sizeof(double)<<std::endl;
+    std::cout<<"char: "<<sizeof(char)<<std::endl;
+    std::cout<<"bool: "<<sizeof(bool)<<std::endl;
+    std::cout<<"long: "<<sizeof(long)<<std::endl;
+    std::cout<<"long long: "<<sizeof(long long)<<std::endl;
+    std::cout<<"short: "<<sizeof(short)<<std::endl;
+    std::cout<<"unsigned int: "<<sizeof(unsigned int)<<std::endl;
+    std::cout<<"unsigned long: "<<sizeof(unsigned long)<<std::endl;
+    std::cout<<"unsigned long long: "<<sizeof(unsigned long long)<<std::endl;
+    std::cout<<"unsigned short: "<<sizeof(unsigned short)<<std::endl;
+    std::cout<<"wchar_t: "<<sizeof(wchar_t)<<std::endl;
+    std::cout<<"size_t: "<<sizeof(size_t)<<std::endl;
+    std::cout<<"ptrdiff_t: "<<sizeof(ptrdiff_t)<<std::endl;
+    std::cout<<"nullptr_t: "<<sizeof(std::nullptr_t)<<std::endl;
+    std::cout<<"void*: "<<sizeof(void*)<<std::endl;
+    std::cout<<"int*: "<<sizeof(int*)<<std::endl;
+    std::cout<<"float*: "<<sizeof(float*)<<std::endl;
+    std::cout<<"double*: "<<sizeof(double*)<<std::endl;
+    std::cout<<"char*: "<<sizeof(char*)<<std::endl;
+    std::cout<<"bool*: "<<sizeof(bool*)<<std::endl;
+    std::cout<<"long*: "<<sizeof(long*)<<std::endl;
+    std::cout<<"long long*: "<<sizeof(long long*)<<std::endl;
+    std::cout<<"short*: "<<sizeof(short*)<<std::endl;
+    std::cout<<"unsigned int*: "<<sizeof(unsigned int*)<<std::endl;
+    std::cout<<"unsigned long*: "<<sizeof(unsigned long*)<<std::endl;
+    std::cout<<"unsigned long long*: "<<sizeof(unsigned long long*)<<std::endl;
+    std::cout<<"unsigned short*: "<<sizeof(unsigned short*)<<std::endl;
+    std::cout<<"wchar_t*: "<<sizeof(wchar_t*)<<std::endl;
+    std::cout<<"size_t*: "<<sizeof(size_t*)<<std::endl;
+    std::cout<<"ptrdiff_t*: "<<sizeof(ptrdiff_t*)<<std::endl;
+    std::cout<<"nullptr_t*: "<<sizeof(std::nullptr_t*)<<std::endl;
+    std::cout<<"void**: "<<sizeof(void**)<<std::endl;
+    return 0;
+}
--- OTHER/test/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "annoying git problems" << std::endl;
-    return 0;
-}
